9f9a574348cf1457a03b619f896eaf8ad63df3af
Amy Cohen <anbcohen@gmail.com>
2018-05-26 10:43:15 -0700

    answered all but the last two questions.  Still pondering on the answers and how to translate what I understand to words

--- lab/server/server.js
+++ lab/server/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT;
 const TOKEN = process.env.TOKEN;
 
 // COMMENT: Explain the following line of code. What is the API_KEY? Where did it come from?
+//The API_Key is an authorization token given to you to be able to use a particular API in a certain way. Sometimes this means you get to make more calls to the API than you would without a key and sometimes it means that you get more access or more featues/information from the API.  You follow the API's documentation to ask for the key. Your information will be linked to it in someway so they can track who is making calls to their service. KEEP IT SECRET!
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 // Database Setup
@@ -29,24 +30,34 @@ app.get('/api/v1/books/find', (req, res) => {
   let url = 'https://www.googleapis.com/books/v1/volumes';
 
   // COMMENT: Explain the following four lines of code. How is the query built out? What information will be used to create the query?
+  //The query is being built out with the title, author, and isbn dynamically.
   let query = ''
+  //This is setting a variable named query, that is currently empty, to hold whatever string you need to put into it
   if(req.query.title) query += `+intitle:${req.query.title}`;
+  //If req.query.title is a thing, then add to that empty string the items in the template literal.
   if(req.query.author) query += `+inauthor:${req.query.author}`;
+  //If req.query.author is a thing, then add to that empty string the items in the template literal.
   if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
+  //If req.query.isbn is a thing, then add to that empty string the items in the template literal.
 
+  //TODO:
   // COMMENT: What is superagent? How is it being used here? What other libraries are available that could be used for the same purpose?
+  //superagent is being used here instead of express (we would say app.get is using express). Superagent is a ajax API.
   superagent.get(url)
     .query({'q': query})
     .query({'key': API_KEY})
     .then(response => response.body.items.map((book, idx) => {
 
       // COMMENT: The line below is an example of destructuring. Explain destructuring in your own words.
+      //Destructuring used here is a way of getting the properties in an object to be individual variables instead of listing them out one at a time (i.e. book.volumeInfo. title, book.volumeInfo.authors, etc.)
       let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
 
       // COMMENT: What is the purpose of the following placeholder image?
+      //When I opened it, it says "No Cover Available", so I'm assuming that if a book does not have a cover image or one that cannot be displayed, then this image would that that place.
       let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
 
       // COMMENT: Explain how ternary operators are being used below.
+      //Ternary is being used to determine the value of the property/what should be displayed. For the title, does the title exisit? If yes, show the title. If not, show the string.  This is also where the placeholder image comes into play that was listed above. If the image_url has a link to a smallThumbnail, use it. Otherwise, use the placeholder.
       return {
         title: title ? title : 'No title available',
         author: authors ? authors[0] : 'No authors available',
@@ -60,6 +71,7 @@ app.get('/api/v1/books/find', (req, res) => {
     .catch(console.error)
 })
 
+//TODO:
 // COMMENT: How does this route differ from the route above? What does ':isbn' refer to in the code below?
 app.get('/api/v1/books/find/:isbn', (req, res) => {
   let url = 'https://www.googleapis.com/books/v1/volumes';
