e11f2caffbad1ad0a7787afbb4a19520b83df127
NateA275 <nateaurich@gmail.com>
2018-05-27 01:13:51 -0700

    All COMMENTS done

--- lab/client/scripts/models/book.js
+++ lab/client/scripts/models/book.js
@@ -54,14 +54,22 @@ var app = app || {};
       .then(() => page('/'))
       .catch(errorCallback)
 
-  // COMMENT: Where is this method invoked? What is passed in as the 'book' argument when invoked? What callback will be invoked after Book.loadAll is invoked?
+  // DONE: Where is this method invoked? What is passed in as the 'book' argument when invoked? What callback will be invoked after Book.loadAll is invoked?
+  /**
+   * This method is called from bookView.initSearchForm. The 'book' can be any or all of title, author, and isbn provided via user input.
+   * The callback that is invoked after loadall is bookView.initSearchResultsPage which will display the results of the search in our Book constructor format.
+   */
   Book.find = (book, callback) =>
     $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/books/find`, book)
       .then(Book.loadAll)
       .then(callback)
       .catch(errorCallback)
 
-  // COMMENT: Where is this method invoked? How does it differ from the Book.find method, above?
+  // DONE: Where is this method invoked? How does it differ from the Book.find method, above?
+  /**
+   * This method is invoked in the bookView.initSearchReusltsPage function when the user clicks the 'Add to List' button.
+   * This method also queries the Google API, however, this one uses the isbn of the selected book, and if the query is sucessful posts the book to the user's database.
+   */
   Book.findOne = isbn =>
     $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/books/find/${isbn}`)
       .then(Book.create)
