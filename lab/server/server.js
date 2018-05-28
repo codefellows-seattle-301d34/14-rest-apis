ce9871935307a8db6674a36ba5ee278418db7f73
Karina Amador <akarina.at@gmail.com>
2018-05-27 19:22:50 -0700

    Added comments to server for final version

--- lab/server/server.js
+++ lab/server/server.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // Application dependencies
 const express = require('express');
@@ -12,6 +12,7 @@ const PORT = process.env.PORT;
 const TOKEN = process.env.TOKEN;
 
 // COMMENT: Explain the following line of code. What is the API_KEY? Where did it come from?
+//This API_Key constant holds the API key value that google provides when the developer is asking for the API. We set it as part of our environtment for security purposes, in order to hide the unique key or identification number so that no one can have access to that number or set of characters.
 const API_KEY = process.env.GOOGLE_API_KEY;
 
 // Database Setup
@@ -23,30 +24,35 @@ client.on('error', err => console.error(err));
 app.use(cors());
 
 // API Endpoints
-app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)))
+app.get('/api/v1/admin', (req, res) => res.send(TOKEN === parseInt(req.query.token)));
 
 app.get('/api/v1/books/find', (req, res) => {
   let url = 'https://www.googleapis.com/books/v1/volumes';
 
   // COMMENT: Explain the following four lines of code. How is the query built out? What information will be used to create the query?
-  let query = ''
+  //Here query has an empty value and its saying that it will get the value of `+intitle:{req.query.title}`, but I'm not reallt sure from where +intitle... is going to take its value.
+  let query = '';
   if(req.query.title) query += `+intitle:${req.query.title}`;
   if(req.query.author) query += `+inauthor:${req.query.author}`;
   if(req.query.isbn) query += `+isbn:${req.query.isbn}`;
 
   // COMMENT: What is superagent? How is it being used here? What other libraries are available that could be used for the same purpose?
+  //superagent is a light client-side HTTP request library. It also allows us to "hide" API keys in our server environment so that no one can access to that unique information.
   superagent.get(url)
     .query({'q': query})
     .query({'key': API_KEY})
     .then(response => response.body.items.map((book, idx) => {
 
       // COMMENT: The line below is an example of destructuring. Explain destructuring in your own words.
+      //Destructuring allows the extraction of objects properties or array values and asign them into another variable. It also uses object literal syntaxis.
       let { title, authors, industryIdentifiers, imageLinks, description } = book.volumeInfo;
 
       // COMMENT: What is the purpose of the following placeholder image?
+      //In the piece of code below, where the return is, there is an image in the view. So if that image exists and comes through successfuly then it will display, if not this placeholderImage will display instead.
       let placeholderImage = 'http://www.newyorkpaddy.com/images/covers/NoCoverAvailable.jpg';
 
       // COMMENT: Explain how ternary operators are being used below.
+      //In my understanding, ternary operators are like if else statements, so for example in the first example: title: title ? title : 'No title available' it means that if there exists a title then show the title, if not then show 'No title available'. And that applies for all of them.
       return {
         title: title ? title : 'No title available',
         author: authors ? authors[0] : 'No authors available',
@@ -54,13 +60,14 @@ app.get('/api/v1/books/find', (req, res) => {
         image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
         description: description ? description : 'No description available',
         book_id: industryIdentifiers ? `${industryIdentifiers[0].identifier}` : '',
-      }
+      };
     }))
     .then(arr => res.send(arr))
-    .catch(console.error)
-})
+    .catch(console.error);
+});
 
 // COMMENT: How does this route differ from the route above? What does ':isbn' refer to in the code below?
+//Here we are looking a book by its isbn identifier, it is more specific. :isbn is kind of like a place holder for an actual isbn number value. So this rout will take us to the view of that specific book and it's properties.
 app.get('/api/v1/books/find/:isbn', (req, res) => {
   let url = 'https://www.googleapis.com/books/v1/volumes';
   superagent.get(url)
@@ -76,11 +83,11 @@ app.get('/api/v1/books/find/:isbn', (req, res) => {
         isbn: industryIdentifiers ? `ISBN_13 ${industryIdentifiers[0].identifier}` : 'No ISBN available',
         image_url: imageLinks ? imageLinks.smallThumbnail : placeholderImage,
         description: description ? description : 'No description available',
-      }
+      };
     }))
     .then(book => res.send(book[0]))
-    .catch(console.error)
-})
+    .catch(console.error);
+});
 
 app.get('/api/v1/books', (req, res) => {
   let SQL = 'SELECT book_id, title, author, image_url, isbn FROM books;';
@@ -92,7 +99,7 @@ app.get('/api/v1/books', (req, res) => {
 app.get('/api/v1/books/:id', (req, res) => {
   let SQL = 'SELECT * FROM books WHERE book_id=$1';
   let values = [req.params.id];
-  
+
   client.query(SQL, values)
     .then(results => res.send(results.rows))
     .catch(console.error);
@@ -100,10 +107,10 @@ app.get('/api/v1/books/:id', (req, res) => {
 
 app.post('/api/v1/books', express.urlencoded(), (req, res) => {
   let {title, author, isbn, image_url, description} = req.body;
-  
+
   let SQL = 'INSERT INTO books(title, author, isbn, image_url, description) VALUES($1, $2, $3, $4, $5);';
   let values = [title, author, isbn, image_url, description];
-  
+
   client.query(SQL, values)
     .then(() => res.sendStatus(201))
     .catch(console.error);
@@ -111,19 +118,19 @@ app.post('/api/v1/books', express.urlencoded(), (req, res) => {
 
 app.put('/api/v1/books/:id', express.urlencoded(), (req, res) => {
   let {title, author, isbn, image_url, description} = req.body;
-  
+
   let SQL = 'UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5 WHERE book_id=$6;';
   let values = [title, author, isbn, image_url, description, req.params.id];
-  
+
   client.query(SQL, values)
     .then(() => res.sendStatus(204))
-    .catch(console.error)
-})
+    .catch(console.error);
+});
 
 app.delete('/api/v1/books/:id', (req, res) => {
   let SQL = 'DELETE FROM books WHERE book_id=$1;';
   let values = [req.params.id];
-  
+
   client.query(SQL, values)
     .then(() => res.sendStatus(204))
     .catch(console.error);
