4c92045f26bfaf0db7debe4cbc8734844b1336ab
Karina Amador <akarina.at@gmail.com>
2018-05-27 19:11:24 -0700

    Finished answer lab 14

--- lab/client/scripts/models/book.js
+++ lab/client/scripts/models/book.js
@@ -14,12 +14,12 @@ var app = app || {};
 
   Book.prototype.toHtml = function() {
     return app.render('book-list-temmplate', this);
-  }
+  };
 
   Book.all = [];
-  
+
   Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
-  
+
   Book.fetchAll = callback =>
     $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/books`)
       .then(Book.loadAll)
@@ -44,7 +44,7 @@ var app = app || {};
       data: book,
     })
       .then(() => page(`/books/${bookId}`))
-      .catch(errorCallback)
+      .catch(errorCallback);
 
   Book.destroy = id =>
     $.ajax({
@@ -52,20 +52,22 @@ var app = app || {};
       method: 'DELETE',
     })
       .then(() => page('/'))
-      .catch(errorCallback)
+      .catch(errorCallback);
 
   // COMMENT: Where is this method invoked? What is passed in as the 'book' argument when invoked? What callback will be invoked after Book.loadAll is invoked?
+  //This method is invoked within initSearchFormPage function and its passing the properties of let book onto its parameter book, and setting initSearchResultsPage as its second parameter.
   Book.find = (book, callback) =>
     $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/books/find`, book)
       .then(Book.loadAll)
       .then(callback)
-      .catch(errorCallback)
+      .catch(errorCallback);
 
   // COMMENT: Where is this method invoked? How does it differ from the Book.find method, above?
+  //This method is invoked in book-view within initSearchResultsPage function. This differs from the function above in the sense that this will find ONE single book which will be found with its isbn property.
   Book.findOne = isbn =>
     $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/books/find/${isbn}`)
       .then(Book.create)
-      .catch(errorCallback)
+      .catch(errorCallback);
 
   module.Book = Book;
-})(app)
+})(app);
--- lab/client/scripts/views/admin-view.js
+++ lab/client/scripts/views/admin-view.js
@@ -13,24 +13,25 @@ var app = app || {};
       let token = event.target.passphrase.value;
 
       // COMMENT: Is the token cleared out of local storage? Do you agree or disagree with this structure?
+      //I don't think localStorage is cleared here.
       $.get(`${app.ENVIRONMENT.apiUrl}/api/v1/admin`, {token})
         .then(res => {
           if(res) {
             localStorage.token = true;
-            page('/');
+            page('/');//I'm not sure of this part. I think its saying that if the request succeeded then the page would display all books but it also would need a sort of verification to get into the admin page.
           } else {
             console.error('Invalid Login.');
           }
         })
         .catch(() => page('/'));
-    })
+    });
   };
 
   adminView.verify = function(ctx, next) {
     if(!localStorage.token) $('.admin').addClass('admin-only');
     else $('.admin').show();
     next();
-  }
+  };
 
   module.adminView = adminView;
-})(app)
+})(app);
